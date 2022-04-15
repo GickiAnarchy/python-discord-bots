daf394a1fe712a7aec0d5925de6c7796f2c875ce
FatherAnarchy <70991592+GickiAnarchy@users.noreply.github.com>
2022-04-15 22:57:02 +0000

    not sure

--- Bots/MTG_Bot/cogs/mtgcog.py
+++ Bots/MTG_Bot/cogs/mtgcog.py
@@ -9,14 +9,13 @@ import time
 from mtgcard import MTGCard, MTGList
 import datetime
 
-
 thisfolder = os.path.abspath(os.path.dirname(__file__))
 setspath = f"{thisfolder}/sets/"
 
 
 class MTGCog(commands.Cog):
     def __init__(self, client):
-        self.urlfile = f"{urls.txt}"
+        self.urlfile = "urls.txt"
         self.client = client
         self.sets = None
         self.codes = []
@@ -32,44 +31,39 @@ class MTGCog(commands.Cog):
         self.urls = []
         self.setupVars()
         self.setupURLs()
-        
-
-
 
     def setupURLs(self):
-      if os.path.exists(f"{self.urlfile}"):
-        with open(f"{self.urlfile}", "r") as f:
-          if not self.urls[0] == None:
-            cpyList = self.urls
-            self.urls = f.readlines()
-            for u in self.urls:
-              if u in cpyList:
-                continue
-              else:
-                self.urls.append(u)
-          else:
-            self.urls = f.readliness()
-          f.close()
+        if os.path.exists(f"{self.urlfile}"):
+            with open(f"{self.urlfile}", "r") as f:
+                if not self.urls[0] == None:
+                    cpyList = self.urls
+                    self.urls = f.readlines()
+                    for u in self.urls:
+                        if u in cpyList:
+                            continue
+                        else:
+                            self.urls.append(u)
+                else:
+                    self.urls = f.readliness()
+                f.close()
 
-          
     def saveURLs(self):
-      if not self.isLoaded:
-        return
-      for c in self.cardsWrap:
-        u = c.imgurl
-        if u == None or "":
-          continue
-        if u in self.urls:
-          continue
-        self.urls.append(f"{u.strip()}")
-      with open(self.urlfile, "w") as f:
-        i = 0
-        for u in self.urls:
-          f.write(u)
-          i += 1
-          print(str(i))
-        f.close()
-
+        if not self.isLoaded:
+            return
+        for c in self.cardsWrap:
+            u = c.imgurl
+            if u == None or "":
+                continue
+            if u in self.urls:
+                continue
+            self.urls.append(f"{u.strip()}")
+        with open(self.urlfile, "w") as f:
+            i = 0
+            for u in self.urls:
+                f.write(u)
+                i += 1
+                print(str(i))
+            f.close()
 
     def setupVars(self):
         if self.sets == None:
@@ -83,25 +77,23 @@ class MTGCog(commands.Cog):
         else:
             self.schedOn = False
 
-
     @commands.command()
     @commands.has_role("Cool Kid")
     async def saveallurls(self, ctx):
-      await ctx.send("this will be a few minutes..", delete_after = 20)
-      self.allcards = Card.where(language = "English").all()
-      await ctx.send("almost done..", delete_after = 2)
-      self.cardsWrap = []
-      for c in self.allcards:
-        wrapped = MTGCard(c)
-        self.cardsWrap.append(wrapped)
-      self.saveURLs()
-
+        await ctx.send("this will be a few minutes..", delete_after=20)
+        self.allcards = Card.where(language="English").all()
+        await ctx.send("almost done..", delete_after=2)
+        self.cardsWrap = []
+        for c in self.allcards:
+            wrapped = MTGCard(c)
+            self.cardsWrap.append(wrapped)
+        self.saveURLs()
 
     @commands.command()
     async def listsets(self, ctx):
         print("listsets")
         await ctx.send("This will take a second....")
-        emb = nextcord.Embed(title = "Sets")
+        emb = nextcord.Embed(title="Sets")
         st = ""
         x = 1
         for s in self.sets:
@@ -114,11 +106,10 @@ class MTGCog(commands.Cog):
                 x = len(self.sets) - 50
             elif x == 50:
                 x = 1
-                emb.add_field(name = "MTG", value = f"{st}")
+                emb.add_field(name="MTG", value=f"{st}")
                 st = ""
-        await ctx.send(embed = emb)
+        await ctx.send(embed=emb)
 
-                
     @commands.command()
     async def choose(self, ctx):
         print("choose")
@@ -126,46 +117,51 @@ class MTGCog(commands.Cog):
             self.codes.append(f"{s.code}")
             self.codenames.append(f"{s.name}")
         await ctx.send("Type in a Set Code:")
+
         def is_correct(m):
-                mesg = str(m.content).upper()
-                if mesg not in self.codes:
-                    print("Not a set code")
-                    return False
-                else:
-                    self.nameindex = self.codes.index(m.content)
-                    return m.author == ctx.author
+            mesg = str(m.content).upper()
+            if mesg not in self.codes:
+                print("Not a set code")
+                return False
+            else:
+                self.nameindex = self.codes.index(m.content)
+                return m.author == ctx.author
+
         try:
-            msg = await self.client.wait_for('message', check=is_correct, timeout=30.0)
+            msg = await self.client.wait_for('message',
+                                             check=is_correct,
+                                             timeout=30.0)
         except asyncio.TimeoutError:
             return await ctx.send("Sorry, I can't wait forever.")
         await ctx.send("gathering cards....")
-        self.cards = Card.where(set = msg.content).where(language = "English").all()        
+        self.cards = Card.where(set=msg.content).where(
+            language="English").all()
         for c in self.cards:
             wrapped = MTGCard(c)
             self.cardsWrap.append(wrapped)
         self.isLoaded = True
         self.saveList.addCards(self.cardsWrap)
-        await ctx.send(f"{self.codenames[self.nameindex]} cards loaded..  Use ..randomCard")
+        await ctx.send(
+            f"{self.codenames[self.nameindex]} cards loaded..  Use ..randomCard"
+        )
 
-
-    @commands.command(aliases = ["showrandom", "rancard", "random"])
+    @commands.command(aliases=["showrandom", "rancard", "random"])
     async def randomCard(self, ctx):
         print("randomCard")
         if self.isLoaded:
             rancard = random.choice(self.cards)
-            em = nextcord.Embed(title = rancard.name)
-            em.set_footer(text = rancard.id)            
+            em = nextcord.Embed(title=rancard.name)
+            em.set_footer(text=rancard.id)
             if rancard.image_url == None:
                 print(f"{rancard.image_url}")
-                rancard.image_url = "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg"                
-            em.set_image(url = rancard.image_url)
+                rancard.image_url = "https://static.wikia.nocookie.net/mtgsalvation_gamepedia/images/f/f8/Magic_card_back.jpg"
+            em.set_image(url=rancard.image_url)
         else:
             await ctx.send("No cards are loaded")
-        await ctx.send(embed = em)
-
+        await ctx.send(embed=em)
 
     @commands.command()
-    async def getCard(self, ctx, isDict = False, sched = False):
+    async def getCard(self, ctx, isDict=False, sched=False):
         print("getCard")
         if self.isLoaded:
             self.myCard = random.choice(self.cardsWrap)
@@ -177,35 +173,31 @@ class MTGCog(commands.Cog):
                 await ctx.send(f"{self.myCard.getDict}")
                 await self.showCard(ctx)
 
-
     @commands.command()
     async def showCard(self, ctx):
         print("showCard")
         if self.myCard == None:
             await ctx.send("You have no card yet")
             return
-        em = nextcord.Embed(title = f"{self.myCard.name}")
-        em.set_image(url = f"{self.myCard.imgurl}")
-        await ctx.send(embed = em)
-
+        em = nextcord.Embed(title=f"{self.myCard.name}")
+        em.set_image(url=f"{self.myCard.imgurl}")
+        await ctx.send(embed=em)
 
     @commands.command()
     async def saveSet(self, ctx):
         print("saveSet")
         if self.isLoaded:
             self.saveList.saveCards()
-            await  ctx.send(f"Set file saved")
-    
-        
-    @commands.command(aliases = ["dlset", "downloadfile", "dlfile"])
+            await ctx.send(f"Set file saved")
+
+    @commands.command(aliases=["dlset", "downloadfile", "dlfile"])
     async def downloadset(self, ctx):
         if not self.isLoaded:
-            file = self.saveList.defaultFile() 
+            file = self.saveList.defaultFile()
         else:
             file = self.saveList.getFile
         nxfile = nextcord.File(file)
-        await ctx.send(file = nxfile)
-
+        await ctx.send(file=nxfile)
 
     @commands.command()
     @commands.has_role("Cool Kid")
@@ -217,42 +209,37 @@ class MTGCog(commands.Cog):
             self.schedOn = False
             print("Scheduled Message is set to FALSE")
 
-
     @commands.command()
     async def HourlyMessage(self, ctx):
         if not self.schedOn:
             await ctx.send("ERROR: schedOn is False. Please set to on.")
         while self.schedOn:
             now = datetime.datetime.now()
-            then = now + datetime.timedelta(hours = 1)
+            then = now + datetime.timedelta(hours=1)
             wait_time = (then - now).total_seconds()
             await self.schedChoose(ctx)
-            await  asyncio.sleep(wait_time)
-            
-            
-    @commands.command(hidden = True)
+            await asyncio.sleep(wait_time)
+
+    @commands.command(hidden=True)
     @commands.has_role("Cool Kid")
     async def schedChoose(self, ctx):
         self.cardsWrap = []
         ranset = random.choice(self.sets)
-        self.cards = Card.where(set = ranset.code).where(language = "English").all()        
+        self.cards = Card.where(set=ranset.code).where(
+            language="English").all()
         for c in self.cards:
             wrapped = MTGCard(c)
             self.cardsWrap.append(wrapped)
         self.isLoaded = True
         self.saveList.addCards(self.cardsWrap)
         print(f"{ranset.name} cards loaded.")
-        await self.getCard(ctx, sched = True)
+        await self.getCard(ctx, sched=True)
         await self.showCard(ctx)
 
-            
-        
-            
-  
 
 ##################################################################
 ##################################################################
+
 
 def setup(client):
     client.add_cog(MTGCog(client))
-    
